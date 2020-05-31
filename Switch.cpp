afa5c4aa9fe1d4dab65d54521a2add7f7b4de717
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-31 10:16:48 -0500

    Deployin a menu

---
+++ Switch.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout<<"1. Save information"<<endl;
+	cout<<"2. Delete information"<<endl;
+	cout<<"3. Edit information"<<endl;
+	cout<<"4. Quit"<<endl;
+
+	
+
+
+	return 0;
+}
